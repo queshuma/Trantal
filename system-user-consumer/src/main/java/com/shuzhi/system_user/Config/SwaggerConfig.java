b43f855046fc344e1fa7b5827235ab5766933d52
queshuma <1428767439@qq.com>
2023-11-21 14:42:55 +0800

    修复了nacos配置中心的bug，添加了Springboot Admin性能监控中心

--- system-user-consumer/src/main/java/com/shuzhi/system_user/Config/SwaggerConfig.java
+++ system-user-consumer/src/main/java/com/shuzhi/system_user/Config/SwaggerConfig.java
@@ -1,15 +1,28 @@
 package com.shuzhi.system_user.Config;
 
+import com.shuzhi.config.SwaggerConfigBase;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 
-//@Configuration
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
 @EnableOpenApi
-public class SwaggerConfig {
+public class SwaggerConfig extends SwaggerConfigBase {
         @Bean
         public Docket docket(){
             return new Docket(DocumentationType.OAS_30)
@@ -17,7 +30,7 @@ public class SwaggerConfig {
                     .enable(true)
                     .groupName("shuzhi")
                     .select()
-                    .apis(RequestHandlerSelectors.basePackage("com.shuzhi.system.com.shuzhi.system_order.Controller"))
+                    .apis(RequestHandlerSelectors.basePackage("com.shuzhi.system_user.Controller"))
 //                    .paths(PathSelectors.ant("/com.shuzhi.system_order.Controller/**"))
                     .build();
         }
@@ -35,4 +48,48 @@ public class SwaggerConfig {
                     "http://www.apache.org/licenses/LICENSE-2.0" //许可证链接
             );
         }
+    @Bean
+    public  Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.soul"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
 }
